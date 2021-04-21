4937e2db8f20ea57130bbbbef5ef37687347110d
Samuel Rey <44244553+SamuelRRR@users.noreply.github.com>
2021-04-21 18:56:43 +0200

    Update erosion.py

--- src/erosion.py
+++ src/erosion.py
@@ -1,5 +1,6 @@
 import math
-import turtle
+import numpy as np
+from matplotlib import pyplot as plt
 
 def matiere(beta, mu, epsilon, tau, kappa, alpha, omega) :
 
@@ -49,171 +50,99 @@ def ligne(epsilon, sigma):
 
 print (ligne(5, 3.6))
 
-def dessin(beta, epsilon, tau, kappa, alpha, sigma) :
+def traitNiveau(epsilon) :
 
-  theta = epsilon * 0.571
+  n = 0
+  res = []
 
-  turtle.Screen().setup(404, 408)
-  turtle.Screen().bgcolor("white")
+  while n < 50 :
+      x = ligne(epsilon, n)
+      res.append(x)
+      n += 1
+      
+  t = np.arange(0, 50)
+  s = res
+  fig, ax = plt.subplots()
+  ax.plot(t, s)
+  ax.set(xlabel='élévation du niveau de la mer (mm/an)', ylabel='recul du trait de côte (m/an)',
+  title='Recul du trait de côte en fonction de la montée du niveau de la mer')
+  ax.grid()
+  fig.savefig("trait-niveau.png")
+  plt.show()
 
-  mer = turtle.Turtle()
-  plage = turtle.Turtle()
-  pluie = turtle.Turtle()
-  vege = turtle.Turtle()
-  const = turtle.Turtle()
+  n = 0
+  res = []
 
-  mer.hideturtle()
-  plage.hideturtle()
-  pluie.hideturtle()
-  vege.hideturtle()
-  const.hideturtle()
+print(traitNiveau(5))
 
-  mer._delay(0)
-  plage._delay(0)
-  pluie._delay(0)
-  vege._delay(0)
-  const._delay(0)
+def traitPente(sigma) :
 
-  mer.pensize(5)
-  plage.pensize(5)
-  pluie.pensize(3)
-  vege.pensize(3)
-  const.pensize(5)
+  n = 1
+  res = []
 
-  mer.color("cyan")
-  if tau == "roche" : plage.color("#2d2f33") and plage.fillcolor("#2d2f33")
-  if tau == "galets" : plage.color("#44474d") and plage.fillcolor("#44474d")
-  if tau == "graviers" : plage.color("#62666e") and plage.fillcolor("#62666e")
-  if tau == "terre" : plage.color("#A0522D") and plage.fillcolor("#A0522D")
-  if tau == "sable" : plage.color("#F5DEB3") and plage.fillcolor("#F5DEB3")
-  pluie.color("blue")
-  const.color("gray")
+  while n < 100 :
+      x = ligne(n, sigma)
+      res.append(x)
+      n += 1
+      
+  t = np.arange(0, 99)
+  s = res
+  fig, ax = plt.subplots()
+  ax.plot(t, s)
+  ax.set(xlabel='inclinaison du littoral (%)', ylabel='recul du trait de côte (m/an)',
+  title="Recul du trait de côte en fonction de l'intensité de la pente littorale")
+  ax.grid()
+  fig.savefig("trait-pente.png")
+  plt.show()
 
-  mer.penup()
-  mer.forward(200)
-  mer.right(90)
-  mer.forward(90-sigma)
-  mer.pendown()
+print(traitPente(3.6))
 
-  mer.begin_fill()
-  mer.right(90)
-  mer.forward(400)
-  mer.left(90)
-  mer.forward(400)
-  mer.setx(200)
-  mer.sety(-90+sigma)
-  mer.end_fill()
+def matierePluie(mu, epsilon, tau, kappa, alpha, omega) :
 
-  plage.penup()
-  plage.forward(200)
-  plage.right(90)
-  plage.forward(100)
-  plage.pendown()
+  n = 0
+  res = []
 
-  plage.begin_fill()
-  plage.right(90+theta)
-  plage.forward(25)
-  m, n = plage.pos()
-  plage.forward(175)
-  u, v = plage.pos()
-  plage.forward(100)
-  a, b = plage.pos()
-  plage.forward(25)
-  c, d = plage.pos()
-  plage.forward(25)
-  e, f = plage.pos()
-  plage.forward(25)
-  g, h = plage.pos()
-  plage.forward(191)
-  plage.left(90+theta)
-  plage.forward(400)
-  plage.setx(200)
-  plage.sety(-100)
-  plage.end_fill()
+  while n < 2000 :
+      x = matiere(n, mu, epsilon, tau, kappa, alpha, omega)
+      res.append(x)
+      n += 1
+      
+  t = np.arange(0, 2000)
+  s = res
+  fig, ax = plt.subplots()
+  ax.plot(t, s)
+  ax.set(xlabel='pluviométrie moyenne (mm/an)', ylabel='déplacement de matière littorale (t/ha/an)',
+  title='Déplacement annuel de matière littorale en fonction de la quantité de pluie annuelle')
+  ax.grid()
+  fig.savefig("matiere-pluie.png")
+  plt.show()
 
-  if beta == 0 : x = 0
-  if beta > 0 and beta <= 300 : x = 400/4
-  if beta > 300 and beta <= 600 : x = 400/8
-  if beta > 600 and beta <= 900 : x = 400/12
-  if beta > 900 and beta <= 1200 : x = 400/16
-  if beta > 1200 and beta <= 1500 : x = 400/20
-  if beta > 1200 : x = 400/24
+  n = 0
+  res = []
 
-  i = 0
+print(matierePluie(30, 5, "sable", "aucune", "aucune", 0.5))
 
-  while i < 400 :
-    pluie.penup()
-    pluie.setheading(300)
-    pluie.setx(-200 + i)
-    pluie.sety(200)
-    pluie.pendown()
-    pluie.forward(20)
-    i += x
-  
-  def tree(x, y) :
-    vege.penup()
-    vege.setheading(90-theta)
-    vege.setx(x)
-    vege.sety(y)
-    vege.pendown()
-    vege.color("brown")
-    vege.forward(20)
-    vege.color("green")
-    vege.right(90)
-    vege.left(45)
-    vege.forward(5)
-    vege.left(180)
-    vege.forward(5)
-    vege.right(180)
-    vege.left(45)
-    vege.forward(5)
-    vege.left(180)
-    vege.forward(5)
-    vege.right(180)
-    vege.left(45)
-    vege.forward(5)
-  
-  def bush(x, y) :
-    vege.penup()
-    vege.setheading(90-theta)
-    vege.setx(x)
-    vege.sety(y)
-    vege.pendown()
-    vege.color("green")
-    vege.right(90)
-    vege.left(45)
-    vege.forward(8)
-    vege.left(180)
-    vege.forward(8)
-    vege.right(180)
-    vege.left(45)
-    vege.forward(8)
-    vege.left(180)
-    vege.forward(8)
-    vege.right(180)
-    vege.left(45)
-    vege.forward(8)
+def matierePente(beta, epsilon, tau, kappa, alpha, omega) :
 
-  if kappa == "quasi nu" : bush(a, b), bush(c, d), bush(e, f), bush(g, h)
-  if kappa == "recouvert" : tree(a, b), tree(c, d), tree(e, f), tree(g, h)
+  n = 0
+  res = []
 
-  if alpha == "protections" :
-    const.pensize(8)
-    const.setheading(270)
-    const.penup()
-    const.setx(m)
-    const.sety(n)
-    const.pendown()
-    const.forward(20)
-    
-  if alpha == "aménagements" :
-    const.setheading(270)
-    const.penup()
-    const.setx(u)
-    const.sety(v)
-    const.pendown()
-    const.forward(5)
+  while n < 100 :
+      x = matiere(beta, n, epsilon, tau, kappa, alpha, omega)
+      res.append(x)
+      n += 1
+      
+  t = np.arange(0, 100)
+  s = res
+  fig, ax = plt.subplots()
+  ax.plot(t, s)
+  ax.set(xlabel='longueur de la pente (m)', ylabel='déplacement de matière littorale (t/ha/an)',
+  title='Déplacement annuel de matière littorale en fonction de la longueur de la pente littorale')
+  ax.grid()
+  fig.savefig("matiere-pente.png")
+  plt.show()
 
-  turtle.Screen().exitonclick()
+  n = 0
+  res = []
 
-print(dessin(300, 10, "sable", "recouvert", "protections", 3.6))
+print(matierePente(600, 5, "sable", "aucune", "protections", 0.5))
